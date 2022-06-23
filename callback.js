58303052c0b1cf31c96fa538065b703c5de93d77
vaivory <100865446+vaivory@users.noreply.github.com>
2022-06-23 21:36:30 +0300

    callback

---
+++ callback.js
@@ -0,0 +1,39 @@
+console.clear();
+
+function sum(a, b, callback) {
+    const ats = a + b;
+    callback(ats);
+}
+
+const numbers = [
+    [2, 4],
+    [7, 9],
+    [-8, 16],
+
+];
+
+//tas kuris gauna i save funkcija tai mes ji vadinsime callbacku
+
+for (const nums of numbers) {
+    const [pirmas, antras] = nums;
+    sum(pirmas, antras, (atsakymas) => {
+        console.log(`${pirmas} + ${antras} = ${atsakymas}`);
+ 
+    })
+}
+
+//callbackus naudojam kai reikia failu sistemos, kuris vyksta per async/awaito
+
+const pirmaFunc=(callbackus) =>{
+    setTimeout(()=>{
+        console.log('Pirma finkcija');
+        callback();
+    }, 1000);
+}
+const antraFunc=() =>{
+    setTimeout(()=>{
+        console.log('Antra finkcija');
+    }, 1000);
+}
+
+pirmaFunc(antraFunc);
